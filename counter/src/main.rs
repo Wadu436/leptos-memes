245ad72aeedfe8c622fa7eff07d2731144957439
Warre Dujardin <warre.dujardin@gmail.com>
2023-04-03 03:56:25 +0200

    switch out actix for axum

--- counter/src/main.rs
+++ counter/src/main.rs
@@ -1,34 +1,33 @@
 #[cfg(feature = "ssr")]
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    use actix_files::Files;
-    use actix_web::*;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use axum::routing::post;
+    use axum::{Router, Server};
     use leptos::*;
-    use leptos_actix::{generate_route_list, LeptosRoutes};
+    use leptos_axum::{generate_route_list, LeptosRoutes};
     use leptos_start::app::*;
+    use std::sync::Arc;
+    use tower_http::services::ServeDir;
+
+    simple_logger::init_with_level(log::Level::Debug).expect("couldn't initialize logging");
 
     let conf = get_configuration(None).await.unwrap();
-    let addr = conf.leptos_options.site_addr;
+    let leptos_options = conf.leptos_options;
+    let addr = leptos_options.site_addr;
     // Generate the list of routes in your Leptos App
-    let routes = generate_route_list(|cx| view! { cx, <App/> });
+    let routes = generate_route_list(|cx| view! { cx, <App/> }).await;
 
-    HttpServer::new(move || {
-        let leptos_options = &conf.leptos_options;
-        let site_root = &leptos_options.site_root;
+    // Axum setup
+    let app = Router::new()
+        .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
+        .leptos_routes(leptos_options.clone(), routes, |cx| view! { cx, <App/> })
+        .fallback_service(ServeDir::new(leptos_options.site_root.clone()))
+        .layer(axum::Extension(Arc::new(leptos_options)));
 
-        App::new()
-            .route("/api/{tail:.*}", leptos_actix::handle_server_fns())
-            .leptos_routes(
-                leptos_options.to_owned(),
-                routes.to_owned(),
-                |cx| view! { cx, <App/> },
-            )
-            .service(Files::new("/", site_root))
-        //.wrap(middleware::Compress::default())
-    })
-    .bind(&addr)?
-    .run()
-    .await
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .map_err(|e| Box::from(e))
 }
 
 #[cfg(not(feature = "ssr"))]
